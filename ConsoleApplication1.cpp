29ca90c34b25859003f8cee6abaa7987cbfdecd9
Rinat Omarov <ruslanasylbek@gmail.com>
2021-03-30 18:09:49 +0600

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <random>
+
+//Вызывает функцию std::cout
+void print()
+{   //Потому что хотим использовать стандартную билиатеку для печати в консоь и
+    //и не использовать какие либо другие библиотеки
+    std::cout << "Hello Skillbox!\n";//This function prints
+}
+/*
+*присваеваем значение пременным и печатаем hello world!
+*/
+
+ int main()
+{
+    int x = 100;
+    3 + 7;
+    x - 100;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+    int mult = x * y;
+    int random = 1002;
+    std::cout << random;  
+}
